a3af5c64b5f6414be1402379ddbcb6d7725e39ed
Ru'a Ayman Abdou <99294398+ruaaabdou1@users.noreply.github.com>
2022-04-02 20:34:40 +0200

    Create BmiScreen.dart

---
+++ BmiScreen.dart
@@ -0,0 +1,354 @@
+# BMI-Calculator
+import 'dart:math' show pow;
+import 'package:flutter/material.dart';
+import 'BmiResult.dart';
+
+class BmiScreen extends StatefulWidget {
+  const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiScreen> createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  bool isMale = true;
+  double height = 120.0;
+  int weight = 40;
+  int age = 10;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF403f3d),
+        centerTitle: true,
+        title: const Text(
+          'BMI Calculator',
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMale = true;
+                        });
+                      },
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Image(
+                              image: AssetImage('assets/images/male.png'),
+                              height: 90,
+                              width: 90,
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Male',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: isMale ? Colors.black54 : Colors.cyan,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMale = false;
+                        });
+                      },
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Image(
+                              image: AssetImage('assets/images/female2.png'),
+                              height: 90,
+                              width: 90,
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Female',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: !isMale ? Colors.black54 : Colors.cyan,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.cyan,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Height',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          ' ${height.round()}',
+                          style: const TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5.0,
+                        ),
+                        const Text(
+                          'CM',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: height,
+                      max: 220,
+                      min: 80,
+                      activeColor: Colors.black,
+                      thumbColor: Colors.black,
+                      inactiveColor: Colors.white,
+                      onChanged: (value) {
+                        setState(() {
+                          height = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.cyan,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'WEIGHT',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${weight.round()}',
+                            style: const TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                backgroundColor: Colors.black,
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                                mini: true,
+                                child: Icon(Icons.remove),
+                                heroTag: 'weight--',
+                              ),
+                              FloatingActionButton(
+                                backgroundColor: Colors.black,
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                },
+                                mini: true,
+                                child: const Icon(Icons.add),
+                                heroTag: 'weight++',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.cyan,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'AGE',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${age.round()}',
+                            style: const TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                backgroundColor: Colors.black,
+                                onPressed: () {
+                                  setState(() {
+                                    age--;
+                                  });
+                                },
+                                mini: true,
+                                child: const Icon(Icons.remove),
+                                heroTag: 'age--',
+                              ),
+                              FloatingActionButton(
+                                backgroundColor: Colors.black,
+
+                                onPressed: () {
+                                  setState(() {
+                                    age++;
+                                  });
+                                },
+                                mini: true,
+                                child: const Icon(
+                                  Icons.add,
+                                ),
+                                heroTag: 'age++',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            color:  const Color(0xFF403f3d),
+            child: MaterialButton(
+              height: 50,
+              onPressed: () {
+                var result = weight / pow(height / 100, 2);
+                var s;
+                if (result>=18&&result<25) {
+                  setState(() {
+                     s = 'normal';
+                  });
+                }
+                else if(result>=25&&result<30){
+                  setState(() {
+                    s='overweight';
+                  });
+                }
+                else if(result>=30&&result<40){
+                setState(() {
+                  s='obesity';
+                });
+                }
+                else if(result>40){
+                 setState(() {
+                   s='morbid obesity';
+                 });
+                }
+                else{
+                  return ;
+                }
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return BmiResult(
+                      age: age,
+                      isMale: isMale,
+                      result: result.round(),
+                      s:s,
+                    );
+                  }),
+                );
+              },
+              child: const Text(
+                'CALCULATE',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
